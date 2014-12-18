afc4767cb0cf837e89fa36755cab6599273c392f
fabiantheblind <icke@fabianmoronzirfas.me>
2014-12-18 10:03:18 +0100

    initial local commit

---
+++ index.php
@@ -0,0 +1,63 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/grid.css" type="text/css" rel="stylesheet" />
+<link href="css/style.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+<title>m&aelig;ve installation</title>
+<script language="javascript" type="text/javascript">
+function roll_over(img_name, img_src)
+   {
+   document[img_name].src = img_src;
+   }
+</script>
+</head>
+<body>
+<div class="container">
+  <div class="content">  
+    <div id="header" class="span-24">
+      <div id="logo" class="span-24"><br/><a href="index.php"> <img src="imgs/maeve_logo.gif" alt="m&aelig;ve" width="208"/> </a> </div>
+      <div class="menu">
+            <span class = "active">m&aelig;ve</span>
+&middot;
+<span ><a href="installation.php">installation</a></span>
+&middot;
+<span ><a href="sponsors.php">sponsors</a></span>
+&middot;
+<span ><a href="credits.php">credits</a></span>
+          </div>
+    </div>
+     <!--end header-->   
+       <div class="bloc span-24">
+      <div class="span-12"> 
+      At the <a href="http://www.labiennale.org/en/architecture/" target="_blank">Venice Biennale</a>, the m&aelig;ve installation connects the entries of the <a href="http://www.labiennale.org/en/architecture/everyville/" target="_blank">EveryVille student competition</a> and puts them into the larger context of  <a href="http://www.mace-project.eu">MACE</a>  content and metadata. By placing physical project cards on an interactive surface, the visitors can explore an organic network of projects, people and media. m&aelig;ve is designed and developed by the <a href="http://interface.fh-potsdam.de">Interface Design team</a> of the University of Applied Sciences Potsdam. 
+      </div>
+ 
+      </div>
+    <div class="homeImg span-24"></div>
+    
+<div  class="txt_bloc span-12"> 
+<object width="470" height="265">	<param name="allowfullscreen" value="true" />	<param name="allowscriptaccess" value="always" />	<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=1738770&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" />	<embed src="http://vimeo.com/moogaloop.swf?clip_id=1738770&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="470" height="265"></embed></object>
+</div>
+<div class="last span-12"><img src="imgs/dummy.gif" height="12px"/></div>
+<div class="last span-12" style="padding-top:7px;">
+	<object width="480" height="258">	<param name="allowfullscreen" value="true" />	<param name="allowscriptaccess" value="always" />	<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=1766968&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" />	<embed src="http://vimeo.com/moogaloop.swf?clip_id=1766968&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="480" height="258"></embed></object>
+	</div>
+</div>
+<div class="span-12 txt_bloc" style="text-align:left;"><a href="installation.php">More pictures and information...</a></div>
+</div>
+
+ <script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+	var pageTracker = _gat._getTracker("UA-2781730-6");
+	pageTracker._trackPageview();
+</script>
+
+</body>
+</html>
